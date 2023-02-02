b192f498a50533fe0f00a682c350148aee513fe9
unknown <ikram9820khan@gmail.com>
2023-02-02 22:07:56 +0500

    course content is completed

---
+++ components/layout/layout.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+import Navigation from './navigation';
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <Navigation />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
+
+export default Layout;
